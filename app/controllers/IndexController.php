4033b656bc48f0520c67a9e661c681a8dc35ba8b
Josue Rosario <josue.rosario@escolar509.sp.senai.br>
2024-03-21 13:37:34 -0300

    rodando apenas controller index - pg9 item 8

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+    namespace App\controllers;
+
+    class IndexController {
+
+        public function index(){
+
+            echo 'Controler: IndexController - Acao: index() <br>';
+    
+        }
+    
+
+    }
+
+
+?>
--- app/route.php
+++ app/route.php
@@ -1,12 +1,42 @@
 <?php
     
-    namespace app;
+    namespace App;
 
     class Route{
 
-        public function getUrl(){
-            return parse_url($_SERVER['REQUEST_URI']);
+        private $routes;
+        private $rota_solicitada;
+
+        public function __construct(){
+            $this->initRoutes(); // Inicia todas as rotas $routes
+            $this->setUrlParam(); // Verifica qual rota o user chamou $rota_solicitada
+            $this->runRoute(); // Roda o controller e método responsavel pela rota
         }
+
+
+        public function initRoutes(){
+            
+            $this->routes['home'] = array(
+                'route' =>  '/',
+                'controller' => 'indexController',
+                'action' => 'index'
+            );    
+        }
+
+        public function setUrlParam(){
+            $this->rota_solicitada = $this->getUrl();
+        }
+
+
+        public function getUrl(){
+            return parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+        }
+
+        public function runRoute(){
+            $indexController = new controllers\IndexController;
+            $indexController->index();
+        }
+
 
     }
 
